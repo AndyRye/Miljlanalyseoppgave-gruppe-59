7166494b71f63662755a027592365525c609d1ac
AndyRye <anders.brekkhus@gmail.com>
2025-04-22 12:10:52 +0200

    Endringer på tester for vær api

--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -6,45 +6,33 @@ import requests
 #Legger til en sti til mappen "data" som inneholder weatherapi.py"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')))
 
-from weatherapi import WeatherAPI
-from weatherapi import WeatherDataProcessor
+from weatherapi import WeatherAPI, WeatherStatistics
 
-class APItestWeather(unittest.TestCase):
+class TestWeatherAPI(unittest.TestCase):
 
     def setUp(self):
         self.api = WeatherAPI()
+        self.api.fetch()
+        self.df = self.api.get_week_view()
 
-    def test_init(self):
-        #Bruker assertEqual for å sjekke at url, parametere og headers stemmer
-        self.assertEqual(self.url, "https://api.met.no/weatherapi/locationforecast/2.0/compact")
-        self.assertEqual(self.params, {"lat" : 59.9139, "lon" : 10.7522 })
-        self.assertEqual(self.headers, {"User-Agent" : "MyWeatherApp/1.0(tvmoen@stud.ntnu.no)"})
-        self.assertIsNone(self.data)
+    def test_data_is_fetched(self):
+        self.assertIsNotNone(self.api.data)
+        self.assertIn("properties", self.api.data, "'properties' mangler i JSON-data")
 
-    
-    def testfetch(self):
-        #Henter feile koordinater og sjekke om det raiser en requests.exceptions.RequestException
-        self.params = {"lat" : 57.9139, "lon" : 10.7522 }
 
-        with self.assertRaises(requests.exceptions.RequestException):
-            self.api.fetch()
+    def test_dataframe_not_empty(self):
+        self.assertFalse(self.df.empty, "DataFrame fra get_week_view er tom")
+        
 
-    def test_get_week_view(self):
-        self.data = None
-        self.get_week_view() 
-        self.assertIsNotNone(self.data)
+    def test_tempertatur_range(self):
+        temps = self.df["Temperatur (C)"]
+        self.assertTrue(temps.between(-50, 50).all(), "Temperatur er utenfor realistisk område")      
 
-    def test_get_month_view(self):
-        self.data = None
-        self.get_month_view()
-        self.assertIsNotNone(self.data)
-
-class WeatherDataProcessor(unittest.TestCase):
-
-    def test_init2(self):
-        test_data = {"relative_humidity": 75.5, "air_temperature": -2.5}
-        client = WeatherAPI(test_data)
-        self.assertEqual(self.data, test_data)
+    def statistics_output(self):
+        stats = WeatherStatistics(self.df)
+        basic_stats = stats.calculate_basic_stats()
+        self.assertIn("Temperatur (C)", basic_stats.column, "Temperaturer mangler i statistikken")
+        self.assertIn("Gjennomsnitt", basic_stats.column, "Gjennomsnitt mangler i statistikken")
 
 
 if __name__ == '__main__':
